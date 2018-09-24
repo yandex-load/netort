6d66cf0a022553a6a8c5531109db444630fdc6b4
noob <noob@yandex-team.ru>
2018-09-24 17:07:48 +0300

    wtf was that?

--- netort/data_manager/clients/lunapark_volta.py
+++ netort/data_manager/clients/lunapark_volta.py
@@ -203,10 +203,6 @@ class LunaparkVoltaClient(AbstractClient):
             key_date=self.key_date,
             local_job_id=self.job.job_id
         ),
-        if req.data.get('sys_uts_offset'):
-            req.data['sys_uts_offset'] = req.data['sys_uts_offset']
-        if req.data.get('log_uts_offset'):
-            req.data['log_uts_offset'] = req.data['log_uts_offset']
         prepared_req = req.prepare()
         logger.debug('Prepared update_job request:\n%s', pretty_print(prepared_req))
         response = send_chunk(self.session, prepared_req)
