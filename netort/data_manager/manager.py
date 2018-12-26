c18d1c49fb306b853cd033c2b86f541fa37c3cae
fomars <fomars@yandex-team.ru>
2018-12-26 14:05:21 +0300

    new_tank_metric

--- netort/data_manager/manager.py
+++ netort/data_manager/manager.py
@@ -58,6 +58,9 @@ class DataSession(object):
     def new_metric(self, meta):
         return self.manager.new_metric(meta)
 
+    def new_tank_metric(self, _type, name, hostname=None, group=None, source=None, *kw):
+        return self.manager.new_tank_metric(_type, name, hostname, group, source, *kw)
+
     def subscribe(self, callback, filter_):
         return self.manager.subscribe(callback, filter_)
 
@@ -146,20 +149,8 @@ class DataManager(object):
         Create and register metric,
         find subscribers for this metric (using meta as filter) and subscribe
 
-        Args:
-            meta (dict): key-value meta information about metric. 'type' required.
-
         Return:
             metric (available_metrics[0]): one of Metric
-
-        meta sample:
-            {
-                'type': 'metrics',
-                'source': 'core',
-                'name': 'cpu_usage',
-                'hostname': 'localhost',
-                'some_meta_key': 'some_meta_value'
-            }
         """
         type_ = meta.get('type')
         if not type_:
@@ -185,6 +176,45 @@ class DataManager(object):
             return metric_obj
         else:
             raise NotImplementedError('Unknown metric type: %s' % type_)
+
+    def new_tank_metric(self, _type, name, hostname=None, group=None, source=None, *kw):
+        """
+        Create and register metric,
+        find subscribers for this metric (using meta as filter) and subscribe
+
+        Return:
+            metric (available_metrics[0]): one of Metric
+        """
+        if not _type:
+            raise ValueError('Metric type should be defined.')
+
+        if _type in available_metrics:
+            metric_obj = available_metrics[_type](None, self.routing_queue)  # create metric object
+            metric_info = {'type': _type,
+                           'source': source,
+                           'name': name,
+                           'hostname': hostname,
+                           'group': group}
+            if kw is not None:
+                metric_info.update(kw)
+            metric_meta = pd.DataFrame({metric_obj.local_id: metric_info}).T  # create metric meta
+            self.metrics_meta = self.metrics_meta.append(metric_meta)  # register metric meta
+            self.metrics[metric_obj.local_id] = metric_obj  # register metric object
+
+            # find subscribers for this metric
+            this_metric_subscribers = self.__reversed_filter(self.subscribers, metric_info)
+            if this_metric_subscribers.empty:
+                logger.debug('subscriber for metric %s not found', metric_obj.local_id)
+            else:
+                logger.debug('Found subscribers for this metric, subscribing...: %s', this_metric_subscribers)
+                # attach this metric id to discovered subscribers and select id <-> callbacks
+                this_metric_subscribers['id'] = metric_obj.local_id
+                found_callbacks = this_metric_subscribers[['id', 'callback']].set_index('id')
+                # add this metric callbacks to DataManager's callbacks
+                self.callbacks = self.callbacks.append(found_callbacks)
+            return metric_obj
+        else:
+            raise NotImplementedError('Unknown metric type: %s' % _type)
 
     def subscribe(self, callback, filter_):
         """
