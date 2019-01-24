bc3309fa1dadf36142a9a2e94ab77fef53157526
fomars <fomars@yandex-team.ru>
2019-01-24 17:42:28 +0300

    +aggr buffer test

--- netort/data_manager/tests/test_router.py
+++ netort/data_manager/tests/test_router.py
@@ -25,4 +25,14 @@ class TestAggregatorBuffer(object):
         assert len(df1) + len(df2) == len(res1) + len(res2) +\
                                       len(self.metrics_router._MetricsRouter__aggregator_buffer.get('metric1', []))
 
-    # def test_buffer_multiple_metrics(self):
+    def test_buffer_multiple_metrics(self):
+        df1 = pd.DataFrame.from_csv('netort/data_manager/tests/df1.csv')
+        df2 = pd.DataFrame.from_csv('netort/data_manager/tests/df2.csv')
+        res11 = self.metrics_router._MetricsRouter__from_aggregator_buffer(df1, 'metric1', False)
+        res21 = self.metrics_router._MetricsRouter__from_aggregator_buffer(df1, 'metric2', False)
+        res12 = self.metrics_router._MetricsRouter__from_aggregator_buffer(df2, 'metric1', False)
+        res22 = self.metrics_router._MetricsRouter__from_aggregator_buffer(df2, 'metric2', True)
+        assert len(df1) + len(df2) == len(res11) + len(res12) + \
+                                      len(self.metrics_router._MetricsRouter__aggregator_buffer.get('metric1', []))
+        assert len(df1) + len(df2) == len(res21) + len(res22)
+        assert self.metrics_router._MetricsRouter__aggregator_buffer.get('metric2', []) is None
