1a933a2ce2f957055f77a2283c02d2ff03f70a0f
fomars <fomars@yandex-team.ru>
2019-03-11 18:32:26 +0300

    v0.4.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='netort',
-    version='0.4.0',
+    version='0.4.1',
     description='common library for yandex-load org',
     longer_description='''
 common library for yandex-load org
